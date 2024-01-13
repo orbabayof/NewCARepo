9441abcd98ca1ee75019393ac4196aa1d563f7d0
claym0re <orbabayof@gmail.com>
2024-01-13 12:48:58 +0200

    Week 2 in CapStone course

---
+++ MetaBack-End/CapStone/CSmain/LittleLemon/restaurant/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Bookin(models.Model):
+     Name = models.CharField(max_length = 255)
+     No_of_guests = models.IntegerField()
+     BookingDate = models.DateField()
+
+class Menu(models.Model):
+     Title = models.CharField(max_length = 255)
+     Price = models.DecimalField(decimal_places = 2,max_digits = 10)
+     Inventory = models.IntegerField()
